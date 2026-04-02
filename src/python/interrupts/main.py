8498542792e0cb2f48e4ed692b22e63ce305668b
Null <VectorZeroAI@gmail.com>
2026-04-02 17:39:45 +0200

    feat: created the interrupt system

---
+++ src/python/interrupts/main.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+import asyncio
+from types import FunctionType
+import functools
+
+INTERRUPT_TABLE = {}
+
+def interrupt(name: str|None = None) -> FunctionType:
+    """
+    register a new interrupt into the interrupt system.
+    Overwrites if the interrupt was present before the operation.
+    """
+    def decorator(func: FunctionType):
+        INTERRUPT_TABLE[name or func.__name__] = func
+        return func
+    return decorator
+
+def interruptible(*q: asyncio.Queue) -> FunctionType:
+    """
+    @interruptible(queue1, queue2)
+    """
+    def decorator(input_func):
+        if isinstance(input_func, type):
+            raise TypeError("@interruptible cannot be used on classes.")
+        if not asyncio.iscoroutinefunction(input_func):
+            raise TypeError("@interruptible requires an async function.")
+
+        
+        @functools.wraps(input_func)
+        async def wrapper(*args, **kwargs):
+            pause_event = asyncio.Event()
+            pause_event.set()
+
+            async def checkpoint():
+                """ The point at wich the function is interruptable by an interrupt. """
+                await pause_event.wait()
+
+            async def listen_to(q: asyncio.Queue):
+                while True:
+                    name = await q.get()
+                    pause_event.clear()
+                    interrupt_handler = INTERRUPT_TABLE.get(name)
+                    if interrupt_handler:
+                        if asyncio.iscoroutinefunction(interrupt_handler):
+                            await interrupt_handler()
+                        else:
+                            await asyncio.to_thread(interrupt_handler)
+                    else:
+                        print(f"Unknown interrupt {name} used")
+                    pause_event.set()
+    
+            listeners = [asyncio.create_task(listen_to(que)) for que in q]
+            try:
+                return await input_func(*args, **kwargs)
+            finally:
+                for i in listeners:
+                    i.cancel()
+        return wrapper
+    return decorator
+

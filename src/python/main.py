44d0af7244e0826667277ba4cd99bf5ee2a861e4
Null <VectorZeroAI@gmail.com>
2026-04-02 17:46:04 +0200

    feat: added error handling

--- src/python/main.py
+++ src/python/main.py
@@ -7,15 +7,17 @@ def main():
     The main function that starts everything 
     Connects to the DB, reads the config, starts the executor cores, and starts the user interface. 
     """
-    conn = psycopg2.connect(
-            host="127.0.0.1",
-            port=5432,
-            dbname="postgres",
-            user="u0_a453"
-            )
-    conn.autocommit = True
-
-    conn.autocommit = True
+    try:
+        conn = psycopg2.connect(
+                host="127.0.0.1",
+                port=5432,
+                dbname="postgres",
+                user="u0_a453"
+                )
+        conn.autocommit = True
+    except Exception as e:
+        raise ConnectionError(f"Connection to the posgres database failed. Reason: {e}") from e
+    
     curr = conn.cursor()
 
     main_file = Path(__file__)
@@ -23,8 +25,14 @@ def main():
     sql_dir = main_file.parent.parent / "sql"
 
     for i in sorted(sql_dir.glob("*.sql")):
-        print(f"executing file {i.name} ...")
-        curr.execute(i.read_text())
+        try:
+            curr.execute(i.read_text())
+        except Exception as e:
+            raise psycopg2.DatabaseError(f"the setup of the db via the sql files failed. reason: {e}") from e
+    
+    
+    
+
     
 if __name__ == "__main__":
     main()

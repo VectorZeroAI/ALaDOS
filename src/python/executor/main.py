8bb7508e4201a7de8f0d908599645829e4cb6ed3
Null <VectorZeroAI@gmail.com>
2026-04-03 10:07:08 +0200

    further work on executor cores

--- src/python/executor/main.py
+++ src/python/executor/main.py
@@ -1,24 +1,19 @@
 #!/usr/bin/env python3
 
+import asyncio
 import threading
 from types import FunctionType
-from typing import Optional, Sequence, TypedDict
+from typing import Sequence
 from ..interrupts.main import interruptable
-from pathlib import Path
 from ..utils.config_dir_resolver import config_dir_resolver
 import tomllib
 from queue import Queue
 import json
 import httpx
 from .types import api, instr_json
+import psycopg2
+import psycopg2.extensions
 
-config_dir = config_dir_resolver()
-
-config_file = config_dir / "executor.toml"
-
-config = tomllib.loads(config_file.read_text())
-
-executor_queue = Queue()
 
 def execute(slave_json: dict): # TODO : ADD a slave json typed dict
     executor_queue.put(slave_json)
@@ -39,6 +34,10 @@ def _llm_call_claude(api: api, instr: str) -> str:
 #        return response.json()["choices"][0]["message"]["content"]
 
 def _llm_call_openai(api: api, instr: str) -> str:
+    """
+    This is the function that calls an openai compatable endpoint.  
+    THE FULL ENDPOINT URL MUST BE PROVIDED, INCLUDING THE v1/completions whatever path.
+    """
     with httpx.Client() as client:
         response = client.post(
                 url=api["url"],
@@ -59,24 +58,35 @@ def llm_call(api: api, instr: str) -> str:
         return _llm_call_openai(api, instr)
 
 @interruptable()
-async def core(checkpoint: FunctionType, queue: Queue, apis: Sequence[api]):
-    instr = queue.get()
-    str_instr = json.dumps(instr)
-    for api in apis:
-        checkpoint()
-        try:
-            result = llm_call(api, str_instr)
-        except httpx.HTTPStatusError:
-            pass
-        else:
-            break
-    
+async def core(checkpoint: FunctionType, queue: Queue, apis: Sequence[api], conn: psycopg2.extensions.connection):
+    while True:
+        await checkpoint()
+        instr = queue.get()
+        str_instr = json.dumps(instr)
+        for api in apis:
+            await checkpoint()
+            try:
+                result = llm_call(api, str_instr)
+                await checkpoint()
+            except httpx.HTTPStatusError:
+                await checkpoint()
+                pass
+            else:
+                await checkpoint()
+                break
 
+def core_thread(coroutine: , queue: Queue, apis: Sequence[api], conn:psycopg2.extensions.connection):
+    loop = asyncio.new_event_loop()
+    try:
+        loop.run_until_complete(coroutine(queue))
 
+def startup(conn: psycopg2.extensions.connection):
+    global executor_queue
+    executor_queue = Queue()
 
+    config_dir = config_dir_resolver()
+    config_file = config_dir / "executor.toml"
+    config = tomllib.loads(config_file.read_text())
 
-
-    
-
-for _ in range(config["cores"]):
-    
+    core_thread = threading.Thread(target=core, args=(executor_queue, config["apis"], conn), daemon=True).start()
+    for _ in range(config["cores_number"]):
--- src/python/executor/types.py
+++ src/python/executor/types.py
@@ -3,12 +3,14 @@
 from typing import TypedDict, Optional
 
 class api(TypedDict):
+    """ An api endpoint representation """
     url: str
     key: str
     model: str
     claude: Optional[bool]
 
 class instr_json(TypedDict):
+    """ An atomic instruction json representation """
     result_addr: int
     instruction: str
     master_addr: int

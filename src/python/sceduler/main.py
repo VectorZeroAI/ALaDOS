93be24aad7d3a0203aea013c21b9731abd499ff4
Null <VectorZeroAI@gmail.com>
2026-04-04 12:22:35 +0200

    started implementing sceduler and the whole task creation / submission pipeline

--- src/python/sceduler/main.py
+++ src/python/sceduler/main.py
@@ -9,5 +9,12 @@ Consists of 1 thread, that listens on the "new_result_inserted" postgres chanell
 Tracks wich tasks are already being executed, and executes all the other tasks. 
 """
 
+from ..utils.conn_factory import conn_factory
+
 def setup():
-    
+    conn = conn_factory()
+    curr = conn.cursor()
+
+    curr.execute("""
+    SELECT 
+                 """)

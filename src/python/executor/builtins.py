83f14aaf7d0e8c9c765ed6d23fe9855787995005
Null <VectorZeroAI@gmail.com>
2026-04-04 08:51:51 +0200

    commit_farming: one code comment change

--- src/python/executor/builtins.py
+++ src/python/executor/builtins.py
@@ -7,4 +7,4 @@ from .execute_tool import register_tool
 def print_to_console(input_str: str) -> None:
     print(input_str)
 
-# TODO : MAKE a friking shitton of tools here, through the whole fucking list
+# TODO : ADD the whole shitload of functions to here.

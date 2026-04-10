3a0b9785e82f3aa6542c43af77cdfd1a43271da2
Null <VectorZeroAI@gmail.com>
2026-04-10 10:44:58 +0200

    feat: made the basic alados server side console work

--- src/python/interfaces/alados_console.py
+++ src/python/interfaces/alados_console.py
@@ -3,10 +3,10 @@ import sys
 import psycopg
 from python.utils.conn_factory import conn_factory
 
-def add_master(instruction: str, conn: psycopg.Connection):
+def add_master(instruction: list[str], conn: psycopg.Connection):
     conn.execute("""
     INSERT INTO masters(instruction) VALUES (%s);
-                 """, (instruction,))
+                 """, (*instruction,))
 
 def start_console():
     """ The function that starts the server side controll console of alados. """
@@ -16,13 +16,13 @@ def start_console():
         command_str = input("admin > ")
         command = command_str.split(" ")
         match command:
-            case "exit", _:
+            case "exit", *_:
                 print("exit is not implemented yet. This terminal will remain utilised until alados server is shut down in the current version") # TODO : Fix this
-            case "shutdown", _:
+            case "shutdown", *_:
                 print("shutdown initiated")
                 sys.exit(0)
-            case "add", "task", _:
-                print(f"adding task {command[2]} as a master goal")
-                add_master(command[2], conn)
+            case "add", "task", *_:
+                print(f"adding task {command[2:]} as a master goal") # TODO: use slicing insdead of raw "2"
+                add_master(command[2:], conn)
             case _:
                 print("undefined command")

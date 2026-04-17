6ff593d65fc221e4ee65445cec1e1d5f3b4e0d6e
Null <VectorZeroAI@gmail.com>
2026-04-17 15:04:32 +0200

    further work on embedder py

--- src/python/executor/embedder.py
+++ src/python/executor/embedder.py
@@ -22,24 +22,46 @@ def setup():
         threading.Thread(target=embedder_thread, daemon=True, args=(embedder_queue,)).start()
 
 def embedder_thread():
+    embedder = None
+
+    def _local_llm_call(text: str):
+        nonlocal embedder
+        if not embedder:
+            from sentense_transformers import SentenseTransformer
+            embedder = 
+
     conn = conn_factory()
     while True:
         item_addr = embedder_queue.get_blocking()
-        desc = conn.execute("""
-    SELECT description FROM viewing_window WHERE addr = %s;
-                 """, (item_addr,)).fetchone()
+        desc_and_type = conn.execute("""
+    SELECT description, type FROM viewing_window vw INNER JOIN addrs_tables at ON at.addr = %s WHERE addr = %s;
+                 """, (item_addr, item_addr)).fetchone()
         if config_method == "local":
             raise NotImplementedError("NOT IMPLEMENTED LOCAL EMBEDDING YET")
         else:
             for i in config_method:
-                pass
+                emb = _call_openai_embedding(i, desc[0])
+                conn.execute(f"""
+        UPDATE {desc[1]} SET emb = %s WHERE addr = %s;
+                             """, (emb, item_addr))
 
-
-
-
-def _call_openai_embedding(api: api):
+def _call_openai_embedding(api: api, text: str):
     with httpx.Client(timeout=15) as client:
-        client.post(
-            url=api['url']
+        response = client.post(
+            url=api['url'],
+            headers={
+                "Content-Type": "application/json",
+                "Autorization": f"Bearer {api['key']}"
+                },
+            json={
+                "input": [text],
+                "model": api['model'],
+                "dimentions": 768,
+                "task": "text-matching",
+                "normalized": True
+                }
+            )
+        response.raise_for_status()
+        emb = response.json()['data'][0]
+    return emb
 
-                )

26d1e05987762d78a1e8fab7ce528ee0c7093b35
Null <VectorZeroAI@gmail.com>
2026-04-18 13:40:19 +0200

    prepared webui addition

---
+++ src/python/interfaces/webui.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""
+This is the file where the whole web UI sever will be. 
+Architecture is simple: 
+    endpoint for the web app
+    endpoint for fetching events
+    websocket for new events. 
+    
+THIS IS PLANNED FOR LATER, AND IS A LOW PRIORITY FEATURE.
+"""

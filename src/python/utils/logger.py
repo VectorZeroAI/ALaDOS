2e2405b89784b17ff38cce37bcb6dc9647e4722a
Null <VectorZeroAI@gmail.com>
2026-04-26 08:31:33 +0200

    fix: logger now works

--- src/python/utils/logger.py
+++ src/python/utils/logger.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from python.utils.conn_factory import conn_factory
 from python.utils.uqueue import Uqueue
+from psycopg.types.json import Jsonb
 import threading
 
 _log_conn = conn_factory()
@@ -13,21 +14,22 @@ def log_json(content: dict) -> None:
 
 def _logger_thread() -> None:
     while True:
+        sleep(0.3)
         items = _logger_queue.get_all()
         try:
             _log_curr.executemany("""
     INSERT INTO logs(content) VALUES(%s);
-                                  """, [(p) for p in items])
+                                  """, [(Jsonb(p),) for p in items])
         except Exception as e:
             print(f"LOGGER THREAD ENCOUNTERED THE FOLLOWING ERROR: {e}, retrying")
             try:
                 _log_curr.executemany("""
         INSERT INTO logs(content) VALUES(%s);
-                                      """, [(p) for p in items])
+                                      """, [(Jsonb(p),) for p in items])
             except Exception as e2:
                 print(f"retry failed because {e2}. Ignoring that. Here are the logs contents for preservation. {items}")
 
 
 def startup():
-    threading.Thread(target=_logger_thread).start()
+    threading.Thread(target=_logger_thread, daemon=True).start()
     print("logger thread started successfully")

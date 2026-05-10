4b35af1611486fb5add5f8f5065a2edad8d51df5
Null <VectorZeroAI@gmail.com>
2026-05-10 10:30:16 +0200

    feat: finishing up the webui backend.

--- src/python/interfaces/webui.py
+++ src/python/interfaces/webui.py
@@ -26,9 +26,9 @@ webserver = Flask(__name__)
 def root_webpage():
     return 'MAIN PAGE! Go to /chat to chat.', 200
 
-@webserver.route('/chat/<session_id>', methods=['POST'])
-def chat_page(session_id: int):
-    return 'PLACEHOLDER PAGE, to be filled in with HTML CSS AND JS later. Also has to somehow get the session.', 200
+@webserver.route('/chat/<session_name>', methods=['POST'])
+def chat_page(session_name: str):
+    return 'PLACEHOLDER PAGE, to be filled in with HTML CSS AND JS later. Also has to somehow pass the session name in.', 200
 
 
 @webserver.route('/_/create_session', methods=['POST'])
@@ -69,20 +69,23 @@ def submit_user_message():
     return jsonify({'success': True}), 200
 
 
-@webserver.route('/_/ai_msg_stream', methods=['POST'])
-def stream_ai_responses():
+@webserver.route("/_/list_session_names_and_ids", methods=["GET", "POST"])
+def list_session_names_and_ids():
     conn = conn_factory()
-    data = request.get_json()
+    session_names_and_ids = _get_session_names_and_ids(conn)
+    return jsonify(session_names_and_ids), 200
 
-    if not data or 'session' not in data:
-        return jsonify({'success': False, 'reason': 'session or json not present in json', 'json': data}), 500
+
+@webserver.route('/_/ai_msg_stream/<session_name>', methods=['GET'])
+def stream_ai_responses(session_name: str):
+    conn = conn_factory()
 
     def ai_messages_yielder():
         conn.execute(
-            sql.SQL("LISTEN {}").format(sql.Identifier(data['session'],)) # NOTE : Dynamic SQL shenanigan
+            sql.SQL("LISTEN {}").format(sql.Identifier(session_name,)) # NOTE : Dynamic SQL shenanigan
         )
         for n in conn.notifies():
-            if not n.channel == data['session']:
+            if not n.channel == session_name:
                 continue
             yield n.payload
 
@@ -91,6 +94,12 @@ def stream_ai_responses():
         mimetype='text/event-stream',
         headers={"Cache-Control": 'no-cache', "X-Accel-Buffering": 'no'}
     )
+
+
+def _get_session_names_and_ids(conn: psycopg.Connection):
+    return conn.execute("""
+SELECT n.name, m.addr FROM masters m JOIN names n ON m.addr = n.addr WHERE n.name LIKE 'session_%'
+                        """).fetchall()
 
 
 def _create_session_sql(first_msg: str, conn: psycopg.Connection):

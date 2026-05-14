9dfb1fc51a0364559a722de0a4bbd86d2afde1cb
Null <VectorZeroAI@gmail.com>
2026-05-14 11:56:43 +0200

    feat: wrote the base of the parser

--- src/python/executor/cronjobs/parser.py
+++ src/python/executor/cronjobs/parser.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
+import time
 from typing import Literal, TypedDict, Any
+
+from ...utils.conn_factory import conn_factory
 
 CronjobActions = Literal['do_this_later', 'notify_user']
 
@@ -8,13 +11,27 @@ class CronjobExpression(TypedDict):
     """ The cronjob expression DSL """
     action: CronjobActions
     params: dict[str, Any]
-    cronjob_frequensy: Literal["loop","once"]
-    time: int
+    cronjob_type: Literal["loop","once"]
+    run_after_or_every_s: int
 
 def parse(input: CronjobExpression):
+    conn = conn_factory()
+    func_body = "def cronjob_function(**kwargs):\n"
+
     match input["action"]:
         case 'do_this_later':
-            pass
+            func_body = func_body + f"    do_this_later({input["params"]})"
+        case 'notify_user':
+            raise NotImplementedError("Not implemented user notifications yet")
+
+    if input['cronjob_type'] == "once":
+        conn.execute("""
+    INSERT INTO cronjob_once(body, start_after) VALUES(%s, %s);
+                     """, (func_body, time.time() + input['run_after_or_every_s']))
+    elif input['cronjob_type'] == "loop":
+        conn.execute("""
+    INSERT INTO cronjob_once(body, execute_every) VALUES(%s, %s);
+                     """, (func_body, input['run_after_or_every_s']))
 
 
 
@@ -24,4 +41,7 @@ def parse(input: CronjobExpression):
 
 
 def do_this_later(ai_instruction: str):
-    pass
+    conn = conn_factory()
+    conn.execute("""
+PERFORM new_slave(NULL, %s);
+                 """, (f"Perform the following actions: '{ai_instruction}'",))

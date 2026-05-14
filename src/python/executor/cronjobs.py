92050eff7c24697105331554afced0c56b44b22f
Null <VectorZeroAI@gmail.com>
2026-05-14 09:55:27 +0200

    feat: working on cronjobs further

--- src/python/executor/cronjobs.py
+++ src/python/executor/cronjobs.py
@@ -1,12 +1,57 @@
 #!/usr/bin/env python3
+from typing import TypedDict
+import psycopg
 from ..utils.conn_factory import conn_factory
 import threading
+import asyncio
+
+class SysState(TypedDict):
+    conn: psycopg.Connection
+
 
 def setup():
     conn = conn_factory()
     
+    threading.Thread(target=cronjob_executor, daemon=True).start()
 
 
-def cronjob_executor():
+async def cronjob_executor():
     conn = conn_factory()
-    conn.execute("LISTEN cronjob_changes")
+    conn.execute("LISTEN 'cronjob_changes'")
+
+    cronjob_changed = asyncio.Future()
+
+    async def cronjob_changes_listener(conn: psycopg.Connection):
+        while True:
+            for n in conn.notifies():
+                cronjob_changed.set_result(True)
+
+    while True:
+
+        sys_state: SysState = {
+            "conn": conn_factory()
+        }
+
+        cronjob = conn.execute("""
+SELECT addr, body, run_at FROM cronjobs_to_run LIMIT 1
+                     """).fetchone()
+        if cronjob is None:
+            await asyncio.wait_for(cronjob_changed, None)
+            continue
+
+        exec(cronjob[1])
+
+        try:
+            await asyncio.wait_for(cronjob_changed, cronjob[2])
+        except TimeoutError:
+            pass
+        else:
+            continue
+
+        try:
+            cronjob_function(sys_state)
+            # NOTE: IGNORE BECAUSE THIS FUNCTION WILL APPEAR AFTER EXEC
+        except NameError:
+            conn.execute("""
+    
+                         """)

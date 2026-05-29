a04fdb99ab093448aa56aaf838628267b46db1cc
Null <VectorZeroAI@gmail.com>
2026-05-29 13:37:31 +0200

    futher work on DSL

--- src/python/rmt/dsl.py
+++ src/python/rmt/dsl.py
@@ -1,30 +1,36 @@
 #!/usr/bin/env python3
 
+from typing import Sequence, TypeAlias, TypedDict
+
 """
 This is the DSL for the rmt.
 For DSL specification, see the documentation.
 """
 
-
 """
+DSL EXAMPLE: 
 START -> (task='task_text', id='lol1212123') -> (task='task_text', id='anything really') -> (task='task_text') -> (task='task_text') -> (task='task_text') -> (task='task_text', id='stuff') -> END
 """
 
-from typing import Sequence, TypeAlias, TypedDict
 
 class RmtNode(TypedDict):
     instruction: str
     id: str|int
     deps: Sequence[str|int]
 
+class RmtNodeIncomplete(RmtNode, total=False):
+    pass
+
 ParsedRmtExpression: TypeAlias = list[RmtNode]
 
 
-def parse(expression: str) -> Sequence[str]:
+def parse(expression: str) -> ParsedRmtExpression:
     """
     The parser function for the RMT expression.
     """
     errors: list[str] = []
+
+    result: ParsedRmtExpression = []
 
     lines = expression.splitlines()
     for line in lines:
@@ -45,7 +51,15 @@ def parse(expression: str) -> Sequence[str]:
                 errors.append(f"Invalid token {token}, tokens must be encapsulated in parenthesis.")
 
         for token in tokens_valid:
+            item: RmtNodeIncomplete = {}
             for key_val in token.split(','):
-                pass
+                for key, val in key_val.split('='):
+                    match key:
+                        case 'instruction':
+                            pass
+                        case 'id':
+                            pass
+                        case _:
+                            errors.append(f"Invalid key found. Key: {key}, key_val pair: {key_val}, token: {token}")
 
     return errors

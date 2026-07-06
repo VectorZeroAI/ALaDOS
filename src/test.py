226628ed7cc38008941ed9471cfdf479c5c8165b
Null <VectorZeroAI@gmail.com>
2026-07-06 17:58:57 +0200

    feat: updated test to test the inserts with params now

---
+++ src/test.py
@@ -0,0 +1,8 @@
+from python.rmt.main import create_from_serial, activate_as_master
+
+DSL_STR = """
+START -> (id='1', instruction='Add "CODE ${{color}}" to the master result')
+"""
+
+addr = create_from_serial(DSL_STR)
+activate_as_master(addr, inputs={'color': "GREEN"})

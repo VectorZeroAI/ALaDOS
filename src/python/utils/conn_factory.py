64f046b98a4b52ec8a867748fa21ab29d79dc91e
Null <VectorZeroAI@gmail.com>
2026-07-11 08:33:23 +0200

    feat: added conviniece methods to conn_factory
    now conn factory has the new conviniece method of "execute_fetchval",
    that makes the entire process of conn.execute(...).fetchone()[0], of
    which pyright complained so much gone into a single good step.

--- src/python/utils/conn_factory.py
+++ src/python/utils/conn_factory.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python3
+
 import psycopg
+from typing import LiteralString, Sequence, cast, Any
+import types
 from psycopg.types import composite
+from psycopg.sql import SQL
 
+class Conn(psycopg.Connection):
+    def execute_fetchval(self) -> Any: ...
 
-def conn_factory() -> psycopg.Connection:
+def _execute_fetchval(self: Conn, querry: SQL|LiteralString, params: Sequence = []) -> Any:
+    tuple_row = self.execute(querry, params).fetchone()
+    if tuple_row:
+        return tuple_row[0]
+    else:
+        raise RuntimeError("Database returned no answer to the querry!")
+
+def conn_factory() -> Conn:
     """
     The factory function for connecting to the database.
     Credentials are hardcoded, because the application sets the DB up internally,
@@ -14,11 +27,14 @@ def conn_factory() -> psycopg.Connection:
             port=5432,
             dbname="alados"
             )
+
     conn.autocommit = True
 
     conn = register_all_the_composite_types(conn)
 
-    return conn
+    conn.execute_fetchval = types.MethodType(_execute_fetchval, conn) # pyright: ignore
+
+    return cast(Conn, conn)
 
 
 def register_all_the_composite_types(conn: psycopg.Connection) -> psycopg.Connection:
@@ -26,5 +42,5 @@ def register_all_the_composite_types(conn: psycopg.Connection) -> psycopg.Connec
     rmt_node_info = composite.CompositeInfo.fetch(conn, "rmt_node")
     assert rmt_node_info is not None
     RmtNodeClass = composite.register_composite(rmt_node_info)
-    conn.RmtNodeClass = RmtNodeClass
+    conn.RmtNodeClass = RmtNodeClass # pyright: ignore
     return conn

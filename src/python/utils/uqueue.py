9ac8f40121c8e44ce62fb093d1f639864c47df93
Null <VectorZeroAI@gmail.com>
2026-07-12 16:08:27 +0200

    feat: updating Uqueue to support adding to the left

--- src/python/utils/uqueue.py
+++ src/python/utils/uqueue.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from typing import TypeVar, Generic
-import queue
+from collections import deque
 import asyncio
 
 T = TypeVar('T')
@@ -11,35 +11,38 @@ class Uqueue(Generic[T]):
     Universal queue, my bloated abomination queue for everything ever.
     """
     def __init__(self):
-        self._queue: queue.Queue[T] = queue.Queue()
+        self._queue: deque[T] = deque()
         
     def put(self, item: T) -> None:
         """ Thread-safe, callable from anywhere """
-        self._queue.put(item)
+        self._queue.append(item)
     
     async def async_get(self) -> T:
         """ Awaitable, non-blocking to the event loop """
         loop = asyncio.get_running_loop()
-        return await loop.run_in_executor(None, self._queue.get)
+        return await loop.run_in_executor(None, self._queue.popleft)
     
     def get(self) -> T:
         """ Escape hatch for non-async contexts """
-        return self._queue.get()
+        return self._queue.popleft()
 
     def get_nowait(self) -> T|None:
         """ Returns an item if available, or None if no items. """
         try:
-            return self._queue.get_nowait()
-        except queue.Empty:
+            return self._queue.popleft()
+        except IndexError:
             return None
 
     def get_all(self) -> list[T]:
         items = []
-        while True:
-            try:
-                items.append(self._queue.get_nowait())
-            except queue.Empty:
-                break
+        while self._queue:
+            items.append(self._queue.popleft())
         return items
+
+    def put_left(self, item: T) -> None:
+        self._queue.appendleft(item)
+
+    def __len__(self) -> int:
+        return len(self._queue)
 
 

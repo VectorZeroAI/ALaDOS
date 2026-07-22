f6ae799f4dfd6ba376d82cd3fbee00ea1763bc8f
Null <VectorZeroAI@gmail.com>
2026-07-22 10:02:12 +0200

    feat: updated the cronjobs to use interrupt based cronjob execution

--- src/python/executor/cronjobs/cronjob_registry.py
+++ src/python/executor/cronjobs/cronjob_registry.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from typing import Any, Callable, TYPE_CHECKING
+from functools import partial
 
 if TYPE_CHECKING:
     from ...executor.cronjobs.main import SysState
@@ -13,6 +14,10 @@ def register_cronjob(name: str) -> Callable:
         return func
     return decorator
 
-def execute_cronjob(name: str, sys_state: SysState, args: dict[str, Any]) -> Any:
+def execute_cronjob(name: str, sys_state: SysState, args: dict[str, Any]) -> None:
     args['sys_state'] = sys_state
     return CRONJOB_REGISTRY[name](**args)
+
+def prepare_cronjob(name: str, sys_state: SysState, args: dict[str, Any]) -> Callable[[], None]:
+    args['sys_state'] = sys_state
+    return partial(CRONJOB_REGISTRY[name], **args)
--- src/python/interrupts/interrupts.py
+++ src/python/interrupts/interrupts.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
+from typing import Callable
+
 from .main import interrupt
+from ..utils.logger import log_json
+from traceback import format_exception
 import time
 
 @interrupt("WAIT")
@@ -10,5 +14,20 @@ def wait_for_rate_limit():
 
 
 @interrupt("execute_cronjob")
-def execute_cronjob():
-    ...
+def execute_cronjob(cronjob: Callable[[], None]):
+    try:
+        cronjob()
+        log_json({
+            "type": 'cronjob',
+            'status': 'normal',
+            'cronjob': str(cronjob.__name__)
+            })
+    except Exception as e:
+        log_json({
+            'type': 'cronjob',
+            'status': 'fatal',
+            'cronjob': str(cronjob.__name__),
+            'error': str(e),
+            'traceback': format_exception(e)
+        })
+        print(f"CRONJOB {cronjob.__name__} failed for reason {e} with traceback {format_exception(e)}")

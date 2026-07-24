92d8b9f60b226a56e364219bb586696fec311b7a
Null <VectorZeroAI@gmail.com>
2026-07-24 10:23:12 +0200

    feat(events/gens): Created am abstraction for event creation

--- src/python/events/event_gens.py
+++ src/python/events/event_gens.py
@@ -2,7 +2,7 @@
 """
 The file where all the event recievers are going to be located in.
 """
-from typing import AsyncGenerator
+from typing import AsyncGenerator, Callable, TypeVar
 
 from asyncinotify import Inotify, Mask
 
@@ -13,8 +13,37 @@ from .types import Event
 
 event_config = load_events_config()
 
+P = TypeVar('P')
+
+def build_event(*parts: P, payload: str, converter: Callable[[P], str]) -> Event:
+    """
+    Constructs the event from the parts.
+    The converter function is used on the parts to convert them to actual parts.
+    We assume parts DONT have separators between eachother, so we '.'.join() them.
+
+    !!!All the event paths are always made full lowercase!!!
+    """
+    
+    parts_new: list[str] = []
+
+    for i in parts: 
+        parts_new.append(converter(i))
+
+    event_path = '.'.join(parts_new)
+    event_path.lower()
+    return Event(event_path, payload)
+    
+
 @register_event_generator("filesystem.fanotify")
 async def filesystem_gen() -> AsyncGenerator[Event, None]:
+    """
+    Event structure: filesystem.delete|create|modify.path.to.file  .
+    """
+
+    def converter(input: str) -> str:
+        """ Converts the path string representation to the NATS event path representation. """
+        return input.replace('/', '.').removeprefix('.')
+
     with Inotify() as inotify:
         for p in event_config.filesystem_watch_dirs:
             inotify.add_watch(p, Mask.DELETE|Mask.CREATE|Mask.MODIFY)
@@ -37,4 +66,5 @@ async def filesystem_gen() -> AsyncGenerator[Event, None]:
                     'message': 'event.name is None.'
                 })
                 continue
-            yield Event('filesystem.' + event.mask.name + event.path.as_posix(), '')
+            yield build_event('filesystem', event.mask.name, event.path.as_posix(), payload='', converter=converter)
+

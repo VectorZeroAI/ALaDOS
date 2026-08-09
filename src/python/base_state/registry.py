7edfe6d93ac2e10d46bb52c2c79ffaf87ddb10ac
Null <VectorZeroAI@gmail.com>
2026-08-09 11:22:19 +0200

    fix(base_state): bugs

--- src/python/base_state/registry.py
+++ src/python/base_state/registry.py
@@ -51,7 +51,7 @@ def register_event_consumer(item: EventConsumers, conn: Conn) -> None:
                  """, (item.addr, item.event_path, item.action_type))
     match item.action_type:
         case "call_rmt":
-            sql = "INSERT INTO event_call_rmt(addr, rmt_addr, args) VALUES(%s, %s::BIGINT, %s)"
+            sql = "INSERT INTO event_call_rmt(addr, rmt_addr, args) VALUES(%s, %s::BIGINT, %s::JSONB)"
         case "execute_slave":
             sql = "INSERT INTO event_call_execute_slave(addr, instruction, scope) VALUES(%s, %s::TEXT, %s::slave_scope)"
         case "fill_result":
@@ -87,7 +87,7 @@ def register_cronjob(item: Cronjob, conn: Conn) -> None:
     else:
         conn.execute("""
         INSERT INTO cronjob_loop(addr, name, args, last_ran, execute_every)
-        VALUES (%s, %s, %s, NOW(), %s);
+        VALUES (%s, %s, %s, (EXTRACT EPOCH FROMNOW()), %s);
                      """, (item.addr, item.action_name, Jsonb(item.args), item.timelapse)) 
 
 
@@ -98,7 +98,7 @@ def register_slaves(item: Slaves, conn: Conn) -> None:
                  """, (item.addr, item.instruction, item.result_addr, item.scope))
     if item.deps:
         conn.executemany("""
-        INSERT INTO slave_req(master_addr, req_addr) VALUES(%s, %s);
+        INSERT INTO slave_req(slave_addr, req_addr) VALUES(%s, %s);
                          """,
                          [(item.addr, d) for d in item.deps],
                          returning=False
@@ -141,16 +141,12 @@ def register_result(item: Results, conn: Conn) -> None:
 @__item_registerer(str(type(Executable)))
 def register_executable(item: Executable, conn: Conn) -> None:
     conn.execute("""
-    DO $$
-    DECLARE 
-        t_addr BIGINT;
-    BEGIN
-        t_addr := %s;
-        INSERT INTO executables(addr, header, body) VALUES (t_addr, %s, %s);
-        INSERT INTO vector_ops(addr, description) VALUES(t_addr, %s);
-    END;
-    $$
-                 """, (item.addr, item.header, item.body, item.description))
+    INSERT INTO executables(addr, header, body) VALUES (%s, %s::TEXT, %s::TEXT);
+                 """, (item.addr, item.header, item.body))
+
+    conn.execute("""
+    INSERT INTO vector_ops(exe_addr, description) VALUES(%s, %s::TEXT);
+                 """, (item.addr, item.description))
     if item.name:
         conn.execute("""
         INSERT INTO names(addr, name) VALUES(%s, %s)
@@ -161,16 +157,12 @@ def register_executable(item: Executable, conn: Conn) -> None:
 @__item_registerer(str(type(Knowledge)))
 def register_knowledge(item: Knowledge, conn: Conn) -> None:
     conn.execute("""
-    DO $$
-    DECLARE
-        t_addr BIGINT;
-    BEGIN
-        t_addr := %s;
-        INSERT INTO knowledge(addr, content) VALUES(t_addr, %s);
-        INSERT INTO vector_ops(addr, content) VALUES(t_addr, %s);
-    END;
-    $$
-                 """, (item.addr, item.content, item.description))
+    INSERT INTO knowledge(addr, content) VALUES(%s, %s::TEXT);
+                 """, (item.addr, item.content))
+
+    conn.execute("""
+    INSERT INTO vector_ops(k_addr, description) VALUES(%s, %s::TEXT);
+                 """, (item.addr, item.description))
     if item.name:
         conn.execute("""
         INSERT INTO names(addr, name) VALUES(%s, %s)

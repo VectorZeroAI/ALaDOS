4d7a0cb2ce4278852faa8314624bceff99cc83cd
Null <VectorZeroAI@gmail.com>
2026-08-10 10:29:02 +0200

    feat(base_state/syscalls): Implemented this chunk of the syscalls.

--- src/python/base_state/state_components/custom_consumers.py
+++ src/python/base_state/state_components/custom_consumers.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
+"""
+The implementation of the lib handshake structure.
+
+The structure:
+    event send to _.syscall.request.<insert_slave_id_here>.<insert_function_name_here>
+    recieve responce from _.syscall.responce.<insert_slave_id_here>.<insert_function_name_here>
+
+Batching results in like "send 50 then recieve all 50" theoretically allowed.
+"""
 
 from ..types import CustomConsumer
 from ..registry import register
@@ -9,17 +18,20 @@ import json
 
 def callback(event: Event):
     syscall_name = event.event_path.split('.')[-1]
+    return_event_path = event.event_path.replace(".request.", ".responce.")
+
     executor_interrupt_queue.put(
         InterruptInvokation(
             "syscall",
             {
                 "function": syscall_name,
-                "args": json.loads(event.payload)
+                "args": json.loads(event.payload),
+                "return_to": return_event_path
             }
         )
     )
 
 CustomConsumer(
-        event_path="_.syscall.*.*",
+        event_path="_.syscall.request.*.*",
         consumer_inner_callback=callback
 )

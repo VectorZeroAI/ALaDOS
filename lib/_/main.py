e63624a5f39fa8dcae9dffbe11c9f76e91657fb7
Null <VectorZeroAI@gmail.com>
2026-08-10 09:51:40 +0200

    feat(lib/internals): created main file

---
+++ lib/_/main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+The internal library main.py file, containing the main logic of the library.
+
+This is the python alados library, containing all of the system calls code required for the
+    DB side to be able to efficiently call kernel functions.
+
+This just means that this file will house the transport layer of the entire library of syscalls.
+"""
+

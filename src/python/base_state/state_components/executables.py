6796b195e85aa6d52cfb39297439d596ec63897d
Null <VectorZeroAI@gmail.com>
2026-08-11 11:40:59 +0200

    feat(base_state/executables): actually registered a test executable.

--- src/python/base_state/state_components/executables.py
+++ src/python/base_state/state_components/executables.py
@@ -6,10 +6,12 @@ Executables base state file, here all the executables in the base state belong.
 from ..types import Executable
 from ..registry import register
 
-Executable(
-    "Test Executable",
-    "print('Test')",
-    "Prints test when executed.",
-    "test_tool",
-    -1
+register(
+    Executable(
+        "Test Executable",
+        "print('Test')",
+        "Prints test when executed.",
+        "test_tool",
+        -1
+    )
 )
